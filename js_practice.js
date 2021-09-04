746d986c1ebd0cfe3afddce05532f9dcadd9216b
Macbook Pro <edcgoziem@gmail.comgit config -- global user.name edcgoziem>
2021-09-04 10:10:57 +0100

    my first project

---
+++ js_practice.js
@@ -0,0 +1,36 @@
+//console.log('helloworld')
+
+console.log(1+1)
+
+console.log(1*1)
+
+console.log(2/5)
+
+console.log(10-30)
+//danny
+
+/* dev practice
+
+w8 */
+
+var firstName = "daniel";
+console.log(firstName)
+
+firstName = "john";
+
+console.log(firstName)
+
+let a = 5
+
+console.log(a);
+
+a = 3 
+ console.log(a)
+
+ const PI = 3.14;
+
+ console.log(PI)
+
+ PI = 3.10;
+
+ console.log(PI); 
