035e0c339209e083aff1033a708bccd38f4b7d27
Macbook Pro <edcgoziem@gmail.comgit config -- global user.name edcgoziem>
2021-09-10 21:15:01 +0100

    added readme

--- js_practice.js
+++ js_practice.js
@@ -31,6 +31,51 @@ a = 3
 
  console.log(PI)
 
- PI = 3.10;
+ //PI = 3.10;
 
  console.log(PI); 
+console.log(5%2)
+console.log(5 ** 20)
+
+var namedan = 1;
+console.log(namedan);
+console.log(namedan++);
+var cyriname = ++cyriname;
+
+console.log(cyriname);
+ var namedan =5;
+
+ namedan = ++namedan
+ console.log(namedan);
+
+
+ // functions
+
+
+  function numAdd(num1, num2) {
+      
+    console.log(num1 + num2)
+  }
+
+  numAdd(1, 2);
+  numAdd(20, 40);
+
+
+  // switch
+
+
+  
+
+
+
+function bigboy(age,money) {
+
+if (age > 10 || money> 1000000){
+
+return 'boy boy';
+} else{
+    return 'small boy'}
+
+}
+  console.log(bigboy(9,200));
+  console.log(bigboy(8,2000));
